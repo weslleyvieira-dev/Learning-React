78fe7404be7bdd9d3635c6317b4f48a878d47e12
Weslley Vieira <wesllivs@gmail.com>
2026-02-10 15:34:42 -0300

    Solve exercise 1.4

--- Exercises/1.4/src/App.js
+++ Exercises/1.4/src/App.js
@@ -60,6 +60,12 @@ export default function App() {
           >
             Related Resources
           </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            React vs JS
+          </button>
         </menu>
         <div id="tab-content">
           <ul>
