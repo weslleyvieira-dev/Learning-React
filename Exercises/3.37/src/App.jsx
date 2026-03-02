526b5f58b3b380804c9addfd2c782ec428e8df83
Weslley Vieira <wesllivs@gmail.com>
2026-03-02 17:50:44 -0300

    Add exercises from 3.49 to 3.60 and replace hardcoded menu items with dynamic rendering

--- Exercises/3.37/src/App.jsx
+++ Exercises/3.37/src/App.jsx
@@ -1,8 +1,17 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES, TOPICS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -10,13 +19,36 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {Object.entries(EXAMPLES).map(([topicId, topicData]) => (
+              <TabButton
+                key={topicId}
+                isSelected={selectedTopic === topicId}
+                onSelect={() => handleSelect(topicId)}
+              >
+                {topicData.title}
+              </TabButton>
+            ))}
+          </menu>
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : (
+            <p>Please select a topic.</p>
+          )}
+        </section>
       </main>
     </div>
   );
