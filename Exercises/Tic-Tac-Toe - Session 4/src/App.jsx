598982e6a7112dacafd0221b3c9fdf5486f9928d
Weslley Vieira <wesllivs@gmail.com>
2026-03-09 17:02:01 -0300

    fix: update winner derivation logic and add disabled state styling for game board buttons

--- Exercises/Tic-Tac-Toe - Session 4/src/App.jsx
+++ Exercises/Tic-Tac-Toe - Session 4/src/App.jsx
@@ -38,20 +38,22 @@ function deriveGameBoard(gameTurns) {
 
 function deriveWinner(totalTurns, gameBoard, players) {
   let winner;
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
+  if (totalTurns >= 5) {
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol =
+        gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol =
+        gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol =
+        gameBoard[combination[2].row][combination[2].column];
 
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
+      if (
+        firstSquareSymbol &&
+        firstSquareSymbol === secondSquareSymbol &&
+        firstSquareSymbol === thirdSquareSymbol
+      ) {
+        winner = players[firstSquareSymbol];
+      }
     }
   }
 
