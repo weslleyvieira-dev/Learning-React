116aeec6f93a461c421e429de8c7970d40708ac0
Weslley Vieira <wesllivs@gmail.com>
2026-04-14 10:04:10 -0300

    feat: add exercises from 10.165 to 10.174

--- Exercises/Shop - Section 10/src/App.jsx
+++ Exercises/Shop - Section 10/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -65,14 +67,23 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
   return (
-    <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+    <CartContext value={ctxValue}>
+      <Header />
+      <Shop onAddItemToCart={handleAddItemToCart}>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext>
   );
 }
 
